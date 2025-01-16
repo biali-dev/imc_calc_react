cb66cc823ae6c7482f5829c134c027773dec85fb
biali <ricardobiali@yahoo.com.br>
2025-01-16 17:04:28 -0300

    atualização

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import TabelaIMC from "./componentes/Tabela/Index";
+import Perfil from "./componentes/Perfil/index";
+import Formulario from "./componentes/Formulario/index";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="divBlock">
+        <Perfil />
+        <section id="result">
+        <Formulario />
+          <section id="reference-table">
+            <TabelaIMC />
+          </section>
+        </section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/componentes/Perfil/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "../Perfil/Perfil.module.css";
+
+const Perfil = () => {
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.name}>CALCULADORA DE IMC</h1>
+    </header>
+  );
+};
+
+export default Perfil;
